97b623b1b5c76131de7e2606eb57737d769142d3
Jb645 <50148003+Jb645@users.noreply.github.com>
2022-04-15 20:14:24 -0400

    Added ability for desktop to keep mod files

--- game/SetUpGame.cmd
+++ game/SetUpGame.cmd
@@ -2,6 +2,8 @@ echo off
 set gamedllPath="release\gamex86.dll"
 set quake2PathLaptop="C:\Program Files (x86)\Steam\steamapps\common\Quake 2\mod"
 
+
+
 if exist %quake2PathLaptop% (
 xcopy /s /y %gamedllPath% %quake2PathLaptop%
 )
@@ -10,4 +12,24 @@ set ShortcutPath="..\customMod\quake2ShortCut"
 
 if exist %quake2PathLaptop% (
 start "" %ShortcutPath%
+)
+
+
+
+
+
+
+set gamedllDesk="release\gamex86.dll"
+set quakePathDesk="D:\SteamLibrary\steamapps\common\Quake 2\mod"
+set gameAddsDesk="..\customMod\Adds"
+
+set ShortcutPathDesk="..\customMod\quake2ShortCutDesk"
+
+if exist %quakePathDesk% (
+xcopy /s /y %gameAddsDesk% %quakePathDesk%
+xcopy /s /y %gamedllDesk% %quakePathDesk%
+)
+
+if exist %quakePathDesk% (
+start "" %ShortcutPathDesk%
 )
